26df037e63e7d4adc5bfaca5f6d2c224b3b2a7d0
louisdangremont <louis.frederic.j.dangremont@vub.be>
2024-12-07 21:57:11 +0000

    trying to solve crazy bugs ..

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    Rails.logger.debug "GOOGLE_MAPS_API_KEY: #{ENV['GOOGLE_MAPS_API_KEY']}"
+  end
+
+  def google_maps_key
+    render json: { API_KEY: ENV['GOOGLE_MAPS_API_KEY'] }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,32 @@
 Rails.application.routes.draw do
+  # Adding logging to monitor route loading
+  Rails.logger.info "Loading routes..."
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  Rails.logger.info "Defining root route"
+  puts "Debug: Root route is being loaded"
+  root "home #index"
+
+  Rails.logger.info "Defining health check route"
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
+  Rails.logger.info "Defining PWA routes"
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  root "home#index"
 
-  # Devise :: https://www.digitalocean.com/community/tutorials/how-to-set-up-user-authentication-with-devise-in-a-rails-7-application
+  # Devise authentication routes
+  Rails.logger.info "Defining Devise routes"
   devise_for :users
 
   # CRUD
-  resources :adventures
-  resources :secrets
+  Rails.logger.info "Defining CRUD routes"
+  resources :adventures [:index]
+  resources :secrets [:index]
+
+  Rails.logger.info "Routes loaded successfully!"
 end
