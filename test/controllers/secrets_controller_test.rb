b542514b7c3dc2979a6b9de1b3584d34585bbc4e
Tundain <106409034+Tundain@users.noreply.github.com>
2024-12-09 17:59:11 +0100

    restore stuff in tests

--- test/controllers/secrets_controller_test.rb
+++ test/controllers/secrets_controller_test.rb
@@ -17,7 +17,7 @@ class SecretsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create secret" do
     assert_difference("Secret.count") do
-      post secrets_url, params: { secret: { body: @secret.body, name: @secret.name } }
+      post secrets_url, params: { secret: { body: @secret.body, name: @secret.name, user_id: @secret.user_id } }
     end
 
     assert_redirected_to secret_url(Secret.last)
