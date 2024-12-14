39670eb7502826f1fe4f2e4c51601a5c366ed1e1
Jonas-Bruell <jonas.bruell@gmail.com>
2024-12-14 12:27:18 +0000

    Vertalen van het merendeel van de tekst op de huidige website

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -2,7 +2,14 @@ class Secret < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  # https://www.youtube.com/watch?v=nqAnftA8LbA
+  # https://www.youtube.com/watch?v=1cw6qO1EYGw
+
   def image_as_thumbnail
     image.variant(resize_to_limit: [200, 200]).processed
   end
+
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :body, presence: true, length: { minimum: 1 }
 end
