3f673bdd5cf4e836978fb0ea3f11208d78bbc5b8
Jonas-Bruell <jonas.bruell@gmail.com>
2024-12-17 09:42:31 +0000

    Updating credentials lookup & fixing flickr api leak (credentials still need to be changed!)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[8.0].define(version: 2024_12_16_155622) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -77,6 +76,10 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_16_155622) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "secret_tags", force: :cascade do |t|
     t.string "name"
@@ -92,9 +95,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_16_155622) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
-    t.text "description"
     t.string "address"
     t.index ["user_id"], name: "index_secrets_on_user_id"
   end
@@ -109,6 +109,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_16_155622) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
