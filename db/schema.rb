906566a563b2e1a50223704e7d76cedf9355e4c2
Tundain <106409034+Tundain@users.noreply.github.com>
2024-12-17 11:08:09 +0100

    fix secrets
    re-add migration files

--- db/schema.rb
+++ db/schema.rb
@@ -91,6 +91,9 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_16_155622) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.text "description"
     t.index ["user_id"], name: "index_secrets_on_user_id"
   end
 
